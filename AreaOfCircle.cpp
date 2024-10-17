5d8d8493fb65dad37b62c94bc1dc6a4f5e62842a
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-17 10:16:53 +0530

    Found area of circle

---
+++ AreaOfCircle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r;
+    cout<<"enter radius number\n";
+    cin>>r;
+    // cout<<"area of square="<<3.14*r*r;
+    cout<<"area of square="<<22.0/7*r*r;
+}
