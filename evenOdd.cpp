0f228a09f03b1d553cf32bb5e0c583508d9e1720
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-18 19:19:37 +0530

    How to find Odd or Even value

---
+++ evenOdd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int number;
+   cout<<"enter a valued number =";
+   cin>>number;
+   if(number%2==0)
+   {
+    cout<<"number is even";
+   }
+   else{
+    cout<<"number is odd";
+   }
+
+}
