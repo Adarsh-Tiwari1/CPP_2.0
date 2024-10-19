20b717a06483433229c1442ef4bbdfa6aa6c7644
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-19 19:07:23 +0530

    Constant value

---
+++ constant.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+   const int a=90;
+    //  a=700;
+    // const int b=60;
+    const int b=10;
+    cout<<a;
+    cout<<"\\n";
+    cout<<"\n";
+    cout<<b;
+    cout<<"\n";
+    cout<<"done";
+}
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    const int a=10;    // const me data type const ke saath he define karte hai.
+    cout<<a;         //   declaring a constant, you should assign its value at the same time
+}
+*/
+
+
+
