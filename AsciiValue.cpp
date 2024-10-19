99eecabbf3a1c83669df96b3bf4bf6d062d3851d
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-19 19:04:38 +0530

    How to print asique value

---
+++ AsciiValue.cpp
@@ -0,0 +1,20 @@
+/*
+#include<iostream>
+using namespace std;
+int main(){
+  char sc;
+  cout<<"";
+  cin>>sc;
+  cout<<(int)sc;
+}
+*/
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    char a;
+    cout<<"";
+    cin>>a;
+    cout<<"Ascii of : "<< a <<" = "<<int(a);
+}
