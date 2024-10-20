98391ec583824962ca8211bc07eedf3aa0a67a06
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-20 12:48:03 +0530

    expression.cpp

---
+++ expression.cpp
@@ -0,0 +1,26 @@
+// Expression operator zero and non zero se comprision karta hai.
+/*
+#include<iostream>
+using namespace std;
+int main(){
+      if(8){         // 0=0   byyy  //  8   welcome
+        cout<<"welcome";  
+      }                   
+      else{
+        cout<<"byy";
+      }
+}
+*/
+
+
+#include<iostream>
+using namespace std;
+int main(){
+  int a;
+  
+  if(3,0,1,0.1,1.0,0.0,0.01,0.1){
+      cout<<"is even";
+  }else{
+      cout<<"is odd";
+  }
+}
