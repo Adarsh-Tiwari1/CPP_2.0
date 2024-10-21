98216f6b68e6c5156cab2f082feb4ba726d29e29
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-21 18:27:36 +0530

    Arithematic Operator

---
+++ Arithmetic.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=90;
+    int b=10;
+    cout<<a+b;
+    cout<<"\n";
+    cout<<a*b;
+    cout<<"\n";
+    cout<<a%b;
+    cout<<"\n";
+    cout<<a-b;
+    cout<<"\n";
+    cout<<a/b;
+}
