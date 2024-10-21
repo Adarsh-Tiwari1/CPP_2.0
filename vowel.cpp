dc328f5ea305d4d9a32879d02eb777b76698d566
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-21 20:23:10 +0530

    Vowel or not

---
+++ vowel.cpp
@@ -0,0 +1,42 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"enter character\n";
+    cin>>ch;
+    if(ch=='a'){
+        cout<<"this is vowel";
+    }
+    else if(ch=='e'){
+        cout<<"this is vowel";
+    }
+    else if(ch=='i'){
+        cout<<"this is vowel";
+    }
+    else if(ch=='o'){
+        cout<<"this is vowel";
+    }
+    else if(ch=='u'){
+        cout<<"this is vowel";
+    }
+    else{
+        cout<<"this is consonant";
+    }
+}
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"enter character\n";
+    cin>>ch;
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+        cout<<"this is vowel";
+    }
+    else{
+        cout<<"this is consonant";
+    }
+}
+*/
