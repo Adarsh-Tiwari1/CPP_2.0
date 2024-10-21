c418df9cf1f56dead0a2a1b69d0b4cc4a1da6bbb
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-21 18:21:11 +0530

    Bitwise Operator

---
+++ BitwiseOperator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+   // a=4 & 5; 
+     // binary me convert karke solve karta hai.
+     // a=7 & 28; 
+    //  a=9 | 13;
+   //  a=7^28;
+
+/*
+   a=12;
+   cout<<~a;
+
+    //cout<<a;
+
+    */
+}
+
+// xor : sam ka zero auro ka one reuten karta hai.
