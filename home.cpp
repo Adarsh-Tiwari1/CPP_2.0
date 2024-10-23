defcb175fa7d3b5fc70e02d7372e7902c1a132b3
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-23 20:00:40 +0530

    some code

---
+++ home.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"";
+    cin>>a;
+    if(cout<<"hello "){
+        cout<<"guys";
+    }
+    else{
+        cout<<"hii";
+    }
+}
