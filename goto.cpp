7e8a026a97151316e66bb67b3d9ed7a16ca2cd62
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-23 20:04:35 +0530

    some

---
+++ goto.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*
+    cout<<"welcome\n";
+    goto cybrom;
+    cout<<"hello\n";
+    cout<<"hello\n";
+    cout<<"hello\n";
+    cybrom:
+    cout<<"byyy\n";
+    */
+    cout<<"welcome\n";
+    cybrom:
+    cout<<"hello\n";
+    cout<<"hello\n";
+    cout<<"hello\n";
+    goto cybrom;
+    cout<<"byyy\n";
+}
+/*
+#include<iostream>
+using namespace std;
+int main(){
+   cout<<"hello";
+   goto adarsh;
+   cout<<"hii";
+   cout<<"hii";
+   adarsh:
+   cout<<"hii";
+}
+*/
