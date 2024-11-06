f3fc094e96e0002220a7bc0a5532298977b9096b
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-06 12:15:09 +0530

    Some Code

---
+++ task.cpp
@@ -0,0 +1,33 @@
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"enter character\n";
+    cin>>ch;
+    if(ch>=97 && ch<=122 ){
+        if(ch=='y'){
+            cout<<" ";
+        }
+        else{
+            cout<<char(ch-32);
+        }
+    }    
+}
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"enter character\n";
+    cin>>ch;
+    if(ch>=97 && ch<=122){
+        if(ch=='a'){
+            cout<<"";
+        }
+        else{
+            cout<<char(ch-32);
+        }
+    }
+}
