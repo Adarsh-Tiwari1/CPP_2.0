930c0bc2b51ddc73119af85cd793376d2f055a71
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-07 23:18:19 +0530

    Basic Cpp

---
+++ basix.cpp
@@ -0,0 +1,18 @@
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int _1$=10;  // variable naming rule convertation 
+    cout<<_1$;
+}
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int a=20;
+    cout<<a;
+}
+*/
