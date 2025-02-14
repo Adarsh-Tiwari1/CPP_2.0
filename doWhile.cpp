49efb7d282bd568bdb446aac17c77b332745c748
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-02-15 00:06:27 +0530

    do while loop

---
+++ doWhile.cpp
@@ -0,0 +1,59 @@
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int a=1;
+    do{
+        if(a%2==0){
+            cout<<a<<"\n";
+        }        
+        a++;
+    }while(a<=10);
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int a=1;
+    do{
+        if(a%2!=0){
+            cout<<a<<"\n";
+        }
+        a++;
+    }while(a<=10);
+}
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int a=1;
+    do{
+         if(a!=6)
+           // break ka use loop se out hone ke liyr karte hai.
+        cout<<a<<"\n";
+         a++;
+        }   
+    while(a<=10);
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int a=1,n;
+    cout<<"";
+    cin>>n;
+    while(a<=n){
+        if(a==10){
+            cout<<"";
+        }else{
+            cout<<a<<"\n";
+        }
+        a++;
+    }
+}
+*/
+
