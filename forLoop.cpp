71002161a71a089250f220985a142b25760db3e1
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-02-16 01:51:21 +0530

    for loop

---
+++ forLoop.cpp
@@ -0,0 +1,208 @@
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=10;i++){
+        cout<<i<<"\n";
+    }    
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=10;i>=-5;i--){
+        cout<<i<<"\n";
+    }    
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    for(i=1;i<=10;i++);
+    {
+        cout<<i<<"\n";
+    } 
+}  
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    for(1;0;1)    // middle wali condition hai jo main hai.wah middle ko expression karta hai
+    {
+        cout<<"byy"<<"\n";
+    } 
+}
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    for(int i=1;i<=255;i++){
+        cout<<i<<"="<<char(i)<<"\t";
+    }
+}
+
+// factor of any number.
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            cout<<i<<"\t";
+        }
+    }
+}
+*/
+
+// wap to find factorial any number.
+/*
+#include<iostream>
+using namespace std;
+int main(){
+   int n,fact=1;
+   cout<<"";
+   cin>>n;
+   for(int i=1;i<=n;i++){
+       fact=fact*i;
+   }
+    cout<<fact;
+}
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int sum=0; 
+    for(int i=1;i<=5;i++){
+        sum=sum+i;
+    }
+    cout<<sum;
+}
+*/
+
+// wap to square root valid number
+/*
+#include<iostream>
+using namespace std;
+int main(){
+     int v;
+     cout<<"";
+     cin>>v;
+     for(int c=1;c<=v/2;c++){
+       if(c*c==v){
+        cout<<c;
+        break;
+       }
+    }    
+}
+*/
+// wap to square root valid number ...
+/*
+#include<iostream>
+using namespace std;
+int main(){
+   int n;
+   cout<<"";
+   cin>>n;
+   for(int i=1;i<=n;i++){
+       if(i*i==n){
+           cout<<i;
+       }
+   }
+}
+*/
+// wap to print table in cpp.
+/*
+#include<iostream>
+using namespace std;
+int main(){
+   int n;
+   cout<<"";
+   cin>>n;
+   for(int i=1;i<=10;i++){
+       cout<<n<<"*"<<i<<"="<<n*i<<"\n";
+   }
+}
+*/
+// sum of n natural num of even number.
+/*
+#include<iostream>
+using namespace std;
+int main(){
+   int n,sum=0;
+   cout<<"";
+   cin>>n;
+   for(int i=1;i<=n;i++){
+       if(i%2==0){
+           sum=sum+i;
+       }
+   }
+   cout<<sum;
+}
+*/
+
+// wap to find factor.
+/*
+#include<iostream>
+using namespace std;
+int main(){
+   int n;
+   cout<<"";
+   cin>>n;
+   for(int i=2;i<=n;i++){
+       if(n%i==0){
+           cout<<i;
+       }       
+   }
+} 
+*/
+// print perfect number .
+/*
+#include<iostream>
+using namespace std;
+int main(){
+   int n,sum=0;
+   cout<<"";
+   cin>>n;
+   for(int i=1;i<n;i++){
+       if(n%i==0){
+           sum=sum+i;
+       }
+   }
+       if(n==sum){
+       cout<<"perfect number";
+   }
+   else{
+       cout<<"not";
+   }
+}  
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=255;i++){
+        cout<<i<<char(i)<<"\t";
+    }
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<5;i++){
+        cout<<i;
+    }     
+}
+*/
+
