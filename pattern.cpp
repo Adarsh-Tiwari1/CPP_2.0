f17016970ec068f36fbdc347ad2a686be2a1e284
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-02-17 22:09:47 +0530

    Pattern Question

---
+++ pattern.cpp
@@ -0,0 +1,124 @@
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    for(int r=1;r<=3;r++){
+        for(int c=1;c<=r;c++){
+            cout<<"*"<<"\t";
+        }
+            cout<<"\n";     
+    }
+}
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    for(int r=1;r<=3;r++){
+        for(int c=1;c<=r;c++){
+            cout<<r<<"\t";
+        }
+            cout<<"\n";     
+    }
+}
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    for(int r=97;r<=99;r++){
+        for(int c=97;c<=r;c++){
+            cout<<char(r)<<"\t";
+        }
+            cout<<"\n";     
+    }
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    for(char r='a';r<='c';r++){
+        for(char c='a';c<=r;c++){
+            cout<<(r)<<"\t";
+        }
+            cout<<"\n";     
+    }
+}
+*/
+  // ask this method ,, 
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int n=1;
+    for(int r=1;r<=3;r++){
+        for(int c=1;c<=r;c++){
+            cout<<n<<"\t";
+            n++;
+        }
+            cout<<"\n";     
+    }
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=4;j++){
+            cout<<i<<"\t";
+        }
+        cout<<"\n";
+    }   
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i<<"\t";
+        }
+        cout<<"\n";
+    }   
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=4;i++){
+        for(int j=4;j>=i;j--){
+            cout<<i<<"\t";
+        }
+        cout<<"\n";
+    }   
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=4;j--){
+            cout<<i<<"\t";
+        }
+        cout<<"\n";
+    }   
+}
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int r=1;r<=5;r++){
+        for(int c=5;c>=r;c--){
+            cout<<"*"<<"\t";
+        }
+        cout<<"\n";
+    }
+}
