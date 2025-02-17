c253922e313a26ac49552468b9801a98929322a1
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-02-18 00:11:19 +0530

    While Loop

---
+++ while.cpp
@@ -0,0 +1,74 @@
+// aagar while loop lga hai galat tarike se to phir o infinite loop print karta hai,
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int a=1;
+    while(0){  // this condition always is false.
+        cout<<a<<"\n";  // this line will never execute.
+        a++;
+    }
+   // cout<<a;
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<=5;cout<<"hello\n"){    
+        i++;
+    }
+}
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    while(1){
+        cout<<"a";
+    }
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int a=1;
+    while(cout<<"good");{   // while loop is never false .
+        cout<<a<<"\n";
+        a--;
+    }
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int a=1,n;
+    cout<<"";
+    cin>>n;
+    while(a<=n){
+        cout<<a<<"\n";
+        a++;
+    }
+}
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int a=1,n;
+    cout<<"";
+    cin>>n;
+    while(a<=n){
+        if(a%2==0){
+            cout<<a<<"\n";
+        }
+        a++;
+    }
+}
+*/
+
+
